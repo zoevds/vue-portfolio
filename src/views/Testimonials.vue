64a0632c990ee8f72dffb675a190e8da2a557829
zoevds <zoevanderschyff13@gmail.com>
2022-07-21 12:18:59 +0200

    commit

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -1,21 +1,5 @@
 <template>
-  <section id="testimonials">
-    <div class="card" style="width: 18rem">
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5></h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
-
-      <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>
-  </section>
+  <section id="testimonials"></section>
 </template>
 <script>
 export default {
